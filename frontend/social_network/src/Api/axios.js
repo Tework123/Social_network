3835851abb470470c6d8d23c932ebc9582cba27a
Denis <domino387@gmail.com>
2023-09-07 14:27:30 +0300

    add virification

--- frontend/social_network/src/Api/axios.js
+++ frontend/social_network/src/Api/axios.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
+axios.defaults.withCredentials = true
+axios.defaults.xsrfCookieName = 'csrftoken'
+axios.defaults.xsrfHeaderName = 'X-CSRFToken'
+
 export default axios.create({
-  baseURL: "http://localhost:8000",
-  withCredentials: true,
-  xsrfCookieName: 'csrftoken',
-  xsrfHeaderName: 'X-CSRFToken'
+  baseURL: "http://127.0.0.1:8000",
 });
