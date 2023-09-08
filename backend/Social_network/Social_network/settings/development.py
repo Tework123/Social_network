349c55aa6e0a365b374c48caec6de4e8e354180c
Tework123 <daniliesaec@gmail.com>
2023-09-08 17:56:30 +0300

    add csrf_trusted_cors

--- backend/Social_network/Social_network/settings/development.py
+++ backend/Social_network/Social_network/settings/development.py
@@ -6,7 +6,7 @@ ALLOWED_HOSTS = []
 DATABASES = {
     'default': {
         'ENGINE': os.environ.get('ENGINE'),
-        'NAME': 'social_network',
+        'NAME': 'social_network1',
         'USER': 'postgres',
         'PASSWORD': os.environ.get('PASSWORD'),
         'HOST': os.environ.get('HOST'),
