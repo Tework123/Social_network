2e6551505f81c3a5d7c88cf5d248b9a8822987fa
Tework123 <daniliesaec@gmail.com>
2023-09-27 07:48:40 +0300

    open db

--- backend/Social_network/Social_network/settings/development.py
+++ backend/Social_network/Social_network/settings/development.py
@@ -3,14 +3,25 @@ from .base import *
 DEBUG = True
 ALLOWED_HOSTS = ['*']
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': os.environ.get('ENGINE'),
+#         'NAME': 'social_network',
+#         'USER': 'postgres',
+#         'PASSWORD': os.environ.get('PASSWORD'),
+#         'HOST': os.environ.get('HOST'),
+#         'PORT': os.environ.get('PORT'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': os.environ.get('ENGINE'),
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'social_network',
         'USER': 'postgres',
-        'PASSWORD': os.environ.get('PASSWORD'),
-        'HOST': os.environ.get('HOST'),
-        'PORT': os.environ.get('PORT'),
+        'PASSWORD': 'ksflkOkas23fl9saflKdl349sLfsk1',
+        'HOST': 'localhost',
+        'PORT': 5432,
     }
 }
 
